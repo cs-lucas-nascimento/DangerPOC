27a2f51b5a679dbb15e46021ce2fbc374e70a628
lucabelezal <lucabelezal@gmail.com>
2019-02-13 00:27:28 -0200

    Add gem install.

--- Script/coveralls.sh
+++ Script/coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 slather coverage -x --output-directory Report/slather/xml_report --scheme DangerPOC DangerPOC.xcodeproj
 slather coverage -s --scheme DangerPOC DangerPOC.xcodeproj
-slather coverage --html --output-directory Report/slather --scheme DangerPOC  DangerPOC.xcodeproj
+slather coverage --html --output-directory Report/slather --scheme DangerPOC DangerPOC.xcodeproj
 slather
