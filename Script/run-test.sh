e9569dee07aa9363e9487779c12aba0d6ae02a0f
lucabelezal <lucabelezal@gmail.com>
2019-02-13 00:17:40 -0200

    Add Script/run-test.sh.

--- Script/run-test.sh
+++ Script/run-test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+# xcodebuild \
+# 	-project Coveralls-iOS.xcodeproj \
+# 	-sdk iphonesimulator \
+# 	-target Coveralls-iOSTests \
+# 	-configuration Debug \
+# 	clean build \
+# 	ONLY_ACTIVE_ARCH=NO \
+# 	TEST_AFTER_BUILD=YES \
+# 	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+# 	GCC_GENERATE_TEST_COVERAGE_FILES=YES
+set -o pipefail && xcodebuild -project DangerPOC.xcodeproj -scheme DangerPOC -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.1' -enableCodeCoverage YES clean build-for-testing test | xcpretty -r "html" -o "Report/build.html"
 
-xcodebuild \
-	-project Coveralls-iOS.xcodeproj \
-	-sdk iphonesimulator \
-	-target Coveralls-iOSTests \
-	-configuration Debug \
-	clean build \
-	ONLY_ACTIVE_ARCH=NO \
-	TEST_AFTER_BUILD=YES \
-	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
-	GCC_GENERATE_TEST_COVERAGE_FILES=YES
